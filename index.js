4e31b825fc9cd302ffaff6d2b4380ee4585c34a2
David Robison <silvapro29@hotmail.com>
2022-03-12 12:44:51 -0300

    timer de estudo

---
+++ index.js
@@ -0,0 +1,38 @@
+function criaSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false, 
+        timeZone: 'UTC'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const parar = document.querySelector('.parar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = criaSegundos(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function (event){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function (event){
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+
+parar.addEventListener('click', function (event){
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
